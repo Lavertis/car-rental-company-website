c45973d0c772b10f74eac16db42b1cd54fb055ce
Lavertis <29688908+Lavertis@users.noreply.github.com>
2021-05-26 22:50:36 +0200

    refactor: divide html code in js function into lines

--- js/history.js
+++ js/history.js
@@ -28,81 +28,46 @@ async function createRentHistory() {
         const rent = rentedCars[i];
         let insurance = "";
         rent.insurance.forEach(el => insurance += `${idToStringMap.get(el)}, `);
+        insurance = insurance.slice(0, -2);
         if (insurance === "")
             insurance = "-";
-        else
-            insurance = insurance.slice(0, -2);
 
-        card += `<div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 mb-4">
-            <div class="card h-100">
-                <img alt="" class="img-fluid card-img-top" src="cars/images/small/${carsData[rent.carName]["photo-path"]}">
-                <div class="card-body">
-                    <h5>${carsData[rent.carName]["name"]}</h5>
-                    <table class="table card-text">
-                        <tr>
-                            <td class="px-1">Rozpoczęcie wynajmu</td>
-                            <td>${rent.startDate}</td>
-                        </tr>
-                        <tr>
-                            <td class="px-1">Zakończenie wynajmu</td>
-                            <td>${rent.endDate}</td>
-                        </tr>
-                        <tr>
-                            <td>Dodatkowe ubezpieczenie</td>
-                            <td>${insurance}</td>
-                        </tr>
-                        <tr>
-                            <td>Odbiór pojazdu</td>
-                            <td>${idToStringMap.get(rent.pickup)}</td>
-                        </tr>
-                        <tr>
-                            <td>Imię</td>
-                            <td>${rent.name}</td>
-                        </tr>
-                        <tr>
-                            <td>Nazwisko</td>
-                            <td>${rent.surname}</td>
-                        </tr>
-                        <tr>
-                            <td>Numer telefonu</td>
-                            <td>${rent.phone}</td>
-                        </tr>
-                        <tr>
-                            <td>Koszt</td>
-                            <td>${rent.price} PLN</td>
-                        </tr>
-                    </table>
+        card += `<div class="col-xl-4 col-lg-6 col-sm-12 mb-4">`;
+        card += `<div class="card h-100">`;
+        card += `<img alt="" class="img-fluid card-img-top" src="cars/images/small/${carsData[rent.carName]["photo-path"]}">`;
+        card += `<div class="card-body">`;
+        card += `<h5>${carsData[rent.carName]["name"]}</h5>`;
+        card += `<table class="table card-text">`;
+        card += `<tr><td>Rozpoczęcie wynajmu</td><td>${rent.startDate}</td></tr>`;
+        card += `<tr><td>Zakończenie wynajmu</td><td>${rent.endDate}</td></tr>`;
+        card += `<tr><td>Dodatkowe ubezpieczenie</td><td>${insurance}</td></tr>`;
+        card += `<tr><td>Odbiór pojazdu</td><td>${idToStringMap.get(rent.pickup)}</td></tr>`;
+        card += `<tr><td>Imię</td><td>${rent.name}</td></tr>`;
+        card += `<tr><td>Nazwisko</td><td>${rent.surname}</td></tr>`;
+        card += `<tr><td>Numer telefonu</td><td>${rent.phone}</td></tr>`;
+        card += `<tr><td>Koszt</td><td>${rent.price} PLN</td></tr>`;
+        card += `</table>`;
+        card += `</div>`;
+        card += `<div class="card-footer d-flex justify-content-between mx-2 bg-white">`;
+        card += `<a class="text-decoration-none my-auto" href="#!"><ul class="list-inline my-auto">`;
 
-                </div>
-                
-                <div class="card-footer d-flex justify-content-between mx-2 bg-white">
-                        <ul class="list-inline my-auto">
-                            <li class="list-inline-item m-0"><a href="#!"><i class="fa fa-star text-success"></i></a>
-                            </li>
-                            <li class="list-inline-item m-0"><a href="#!"><i class="fa fa-star text-success"></i></a>
-                            </li>
-                            <li class="list-inline-item m-0"><a href="#!"><i class="fa fa-star text-success"></i></a>
-                            </li>
-                            <li class="list-inline-item m-0"><a href="#!"><i class="fa fa-star text-success"></i></a>
-                            </li>
-                            <li class="list-inline-item m-0"><a href="#!"><i class="fa fa-star text-success"></i></a>
-                            </li>
-                        </ul>
-                        <button class="btn btn-primary btn-warning text-white deleteBtn" id="deleteBtn${i}"
-                                type="button">Usuń
-                        </button>
-                    </div>
-            </div>
-        </div>`;
+        // <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+        // <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+        // <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+        // <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+        // <li class="list-inline-item m-0"><i class="fa fa-star text-success"></i></li>
+
+        card += "</a></ul>";
+        card += "<button class='btn btn-primary btn-warning text-white deleteBtn' id='deleteBtn${i}'>Usuń</button>";
+        card += "</div></div></div>";
     }
 
     $("#rent-history-cards").html(card);
     $(".deleteBtn").each(function (index, obj) {
-        console.log(index);
-        obj.addEventListener("click", async function () {
+        obj.addEventListener("click", function () {
             if (window.confirm("Czy na pewno chcesz usunąć?")) {
                 removeFromLocalStorage(index);
-                await createRentHistory();
+                createRentHistory();
             }
         });
     })
@@ -130,8 +95,8 @@ function insertSampleDataToLocalStorage() {
     rent1.pickup = "self-pickup";
     rent1.startDate = "2021-08-10";
     rent1.endDate = "2021-08-15";
-    rent1.name = "Adam";
-    rent1.surname = "Kozłowski";
+    rent1.name = "Katarzyna";
+    rent1.surname = "Kozłowska";
     rent1.phone = "999-999-999";
     rent1.price = 1200;
     rentedCars.push(rent1);
